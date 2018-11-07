822a4b941dff4e72c92fed7856fd1468de2018d3
Ushinji <siguma971@gmail.com>
2018-11-08 02:28:28 +0900

    flake8の設定

--- app/views/health_check_view.py
+++ app/views/health_check_view.py
@@ -1,5 +1,6 @@
 from app import application
 
+
 @application.route('/health_check', methods=['GET'])
 def health_check():
     return '', 200, {}
--- app/views/session_view.py
+++ app/views/session_view.py
@@ -1,6 +1,7 @@
 from flask import redirect, render_template
 from app import application
 
+
 @application.route('/', methods=['GET'])
 def index():
     return render_template('session/index.html')
